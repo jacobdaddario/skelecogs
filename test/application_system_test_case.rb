8bd1037040d75bf6de3e898ffefb0d0696b7395c
Jacob Daddario <jacob.d@hey.com>
2022-04-10 12:43:22 -0500

    Got cuprite config to work correctly

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,21 +1,8 @@
 require "test_helper"
 require "capybara/cuprite"
 
-Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(
-    app,
-    **{
-      # Enable debugging capabilities
-      inspector: true,
-      headless: !ENV["HEADLESS"].in?(%w[n 0 no false])
-    }
-  )
-end
-
-Capybara.default_driver = Capybara.javascript_driver = :cuprite
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :cuprite, using: :chrome, screen_size: [1400, 1400]
+  driven_by :cuprite, using: :chrome, screen_size: [1400, 1400], options: { inspector: true, headless: !ENV["HEADLESS"].in?(%w[n 0 no false])}
 
   def with_preview(preview_name)
     component_name = self.class.name.gsub("Test", "").gsub("Integration", "")
