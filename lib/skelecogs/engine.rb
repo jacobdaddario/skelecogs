bbcf20be30d4a3450883fb517489bde49997ca91
Jacob Daddario <jacob.d@hey.com>
2022-04-10 11:21:31 -0500

    Removed source of deprecations

--- lib/skelecogs/engine.rb
+++ lib/skelecogs/engine.rb
@@ -2,7 +2,7 @@ module Skelecogs
   class Engine < ::Rails::Engine
     require "turbo-rails"
     require "stimulus-rails"
-    require "view_component/engine"
+    require "view_component"
 
     initializer "skelecogs.assets.precompile" do |app|
       app.config.assets.precompile << "assets/builds/application.js"
