97f4b42a841467102fa4fcd6ea5bae607ce9d08d
Jacob Daddario <jacob.d@hey.com>
2022-04-30 10:52:55 -0500

    Show is now a required input of transition components

--- app/components/skelecogs/transition_component.rb
+++ app/components/skelecogs/transition_component.rb
@@ -1,5 +1,13 @@
 module Skelecogs
   class TransitionComponent < ApplicationComponent
+    def initialization_hook(opts)
+      @visible = opts[:show]
+
+      if @visible.nil?
+        throw ArgumentError.new("Show is a required input of transition components.")
+      end
+    end
+
     private
 
     def default_tag
--- test/components/transition_component_test.rb
+++ test/components/transition_component_test.rb
@@ -6,7 +6,7 @@ class TransitionComponentTest < ViewComponent::TestCase
   include ActionView::Helpers::TagHelper
 
   test "should render without crashing" do
-    render_inline Skelecogs::TransitionComponent.new do |c|
+    render_inline Skelecogs::TransitionComponent.new(show: true) do |c|
       tag.div "Children", class: "hello"
     end
 
@@ -14,8 +14,16 @@ class TransitionComponentTest < ViewComponent::TestCase
   end
 
   test "should be possible to render a transition without children" do
-    render_inline Skelecogs::TransitionComponent.new(class: "transition")
+    render_inline Skelecogs::TransitionComponent.new(show: true, class: "transition")
 
     assert_selector ".transition"
+  end
+
+  test "should throw an argument error when the required show argument is not provided" do
+    assert_raise ArgumentError do
+      render_inline Skelecogs::TransitionComponent.new do |c|
+        tag.div "Children", class: "hello"
+      end
+    end
   end
 end
