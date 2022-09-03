84752dd964a0ab8192e01cd37b21ab162e223b14
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-03 22:12:35 +0800

    Delete app directory

--- app/javascript/controllers/index.js
+++
@@ -1,6 +0,0 @@
-import { application } from "./application"
-
-import DropdownController from "./dropdown_controller"
-application.register("dropdown", DropdownController)
-import PrefixedIdController from "./prefixed_id_controller"
-application.register("prefixed-id", PrefixedIdController)
