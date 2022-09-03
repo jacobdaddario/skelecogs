84e27cab0059c7dd3cab8a4a319ad77b8df33728
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-03 22:14:25 +0800

    Delete test directory

--- test/skelecogs_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SkelecogsTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Skelecogs::VERSION
-  end
-end
--- test/utils/accessability_helpers.rb
+++
@@ -1,9 +0,0 @@
-module AccessabilityHelpers
-  def get_active_element
-    page.driver.evaluate_script("document.activeElement")
-  end
-
-  def assert_active_element(element)
-    assert_equal element.native, get_active_element, "#{element} was not the active element"
-  end
-end
